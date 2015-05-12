01e6e8ab523cb28ad636747f6497847f4aa25410
Fen1kz <ffen1kz@gmail.com>
2015-05-12 03:04:20 +0300

    hero

---
+++ directive/attributes-traits/attributes-traits.js
@@ -0,0 +1,90 @@
+angular.module('AndProcRLData').directive('attributesTraits', function($timeout, OnRepeatEvents, dataService, attributeService) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+
+		},
+		templateUrl: 'directive/attributes-traits/attributes-traits.html',
+		link: function($scope, element, attrs, fn) {
+            $scope.attributes = dataService.data.attributes;
+            $scope.traits = dataService.data.traits;
+            $scope.links = [];
+
+            var attrsx = 0;
+            var attrs_width = 100;
+            var traitsx = 200;
+            var stepy = 40;
+
+            _.each($scope.traits, function(trait, index){
+                trait.gfx.x = traitsx;
+                trait.gfx.y = stepy * index;
+                $scope.$watch('traits['+index+'].formula', function(formula) {
+                    if (formula) {
+                        _.each($scope.attributes, function(attr, attrIndex) {
+                            formula = formula.replace(attr.id, '(+dataService.data.attributes['+attrIndex+'].value || 0)');
+                        });
+                        try {
+                            trait.fn = eval('(function(){return ' + formula + ';})');
+                            trait.value = trait.fn();
+                        } catch (e) {
+                            //console.log('Wrong formula');
+                        }
+                        $scope.refreshLinks();
+                    }
+                });
+            });
+
+            _.each($scope.attributes, function(e, index){
+                e.gfx.x = attrsx;
+                e.gfx.y = stepy * index;
+                $scope.$watch('attributes['+index+'].value', function() {
+                    _.each($scope.traits, function(trait) {
+                        try {
+                            trait.value = trait.fn ? trait.fn() : 0;
+                        } catch (e) {
+                            //console.log('Wrong formula (attr.value changed)');
+                        }
+                    });
+                });
+            });
+
+            //$scope.$on(OnRepeatEvents.LAST, function(event, element, attrs){
+            //$scope.$evalAsync(function(event, element, attrs){
+            $timeout(function(){
+                $scope.$watch('links', function(newValue) {
+                    _.each($scope.links, function(link) {
+                        var attr = $scope.attributes[link.from.index];
+                        var $attr = $('[data-attr="'+attr.id+'"]');
+                        var trait =  $scope.traits[link.to.index];
+                        var $trait =  $('[data-trait="'+trait.id+'"]');
+                        var ax = attr.gfx.x + $attr.outerWidth() + 5,
+                            ay = attr.gfx.y + $attr.outerHeight() / 2,
+                            bx = trait.gfx.x,
+                            by = trait.gfx.y + $trait.outerHeight() / 2,
+                            dx = ax - bx,
+                            dy = ay - by,
+                            angle = Math.atan2(dy, dx) * 180 / Math.PI + 90,
+                            length = Math.sqrt(dx * dx + dy * dy);
+
+                        link.style = {
+                            width: 1,
+                            height: length,
+                            'background-color': 'black',
+                            position: 'absolute',
+                            left: ax + 'px',
+                            top: ay + 'px',
+                            transform: 'rotate('+angle+'deg)',
+                            'transform-origin': '0% 0%'
+                        };
+                    });
+                });
+            });
+
+            $scope.refreshLinks = function() {
+                $scope.links = attributeService.refreshLinks();
+            };
+            $scope.refreshLinks();
+		}
+	};
+});
--- partial/home/home.js
+++ partial/home/home.js
@@ -2,92 +2,16 @@ angular.module('AndProcRLData')
     .controller('HomeCtrl', function ($scope, dataService, charClassService, attributeService) {
         $scope.childClassesIndexes = charClassService.getClassesIndexes(undefined);
 
-        $scope.attributes = dataService.data.attributes;
-        $scope.traits = dataService.data.traits;
-        $scope.links = [];
-
-        _.each($scope.traits, function(trait, index){
-            trait.gfx.x = 400;
-            trait.gfx.y = 30 * (index + 1);
-            $scope.$watch('traits['+index+'].formula', function(newValue) {
-                if (newValue) {
-                    trait.fn = makeFn(newValue);
-                    try {
-                        trait.value = trait.fn();
-                    } catch (e) {
-                        console.log('wrong!!!!!!!!!');
-                    }
-                }
-            });
-        });
-        _.each($scope.attributes, function(e, index){
-            e.gfx.x = 100;
-            e.gfx.y = 30 * (index + 1);
-            $scope.$watch('attributes['+index+'].value', function() {
-                _.each($scope.traits, function(trait) {
-                    try {
-                        trait.value = trait.fn ? trait.fn() : 0;
-                    } catch (e) {
-                        console.log('wrong!!!!!!!!!');
-                    }
-                });
-            });
-        });
-
-        function makeFn(formula) {
-            _.each(dataService.data.attributes, function(attr, attrIndex) {
-                formula = formula.replace(attr.id, '(+dataService.data.attributes['+attrIndex+'].value || 0)');
-            });
-            var fn;
-            try {
-                fn = eval('(function(){return ' + formula + ';})');
-            } catch (err) {
-            }
-            return fn;
-        }
-
-        $scope.refreshLinks = function() {
-            $scope.links = attributeService.refreshLinks();
-        };
-        $scope.refreshLinks();
-
-        $scope.$watch('links', function(newValue) {
-            _.each($scope.links, function(link){
-                var line = linedraw(
-                    $scope.attributes[link.from.index].gfx.x+200,
-                    $scope.attributes[link.from.index].gfx.y,
-                    $scope.traits[link.to.index].gfx.x,
-                    $scope.traits[link.to.index].gfx.y
-                );
-                link.style = {
-                    width: 1,
-                    height: line.length,
-                    'background-color': 'black',
-                    position: 'absolute',
-                    left: line.ax + 'px',
-                    top: line.ay + 'px',
-                    transform: 'rotate('+line.calc+'deg)',
-                    'transform-origin': '0% 0%'
-                };
-                console.log(link.style);
-            });
-        });
-
-        function linedraw(ax, ay, bx, by) {
-            //if (ay > by) {
-                var dx = ax - bx;
-                var dy = ay - by;
-            //}
-            return {
-                ax: ax,
-                ay: ay,
-                calc: Math.atan2(dy, dx) * 180 / Math.PI + 90,
-                length: Math.sqrt(dx * dx + dy * dy)
-                };
-        }
-
         $scope.flush = function () {
             charClassService.flush();
             dataService.flush('data');
         };
+
+        $scope.hero = {
+            class: charClassService.data['Adventurer'],
+            level: 1
+        };
+        $scope.classes = charClassService.data;
+        $scope.attributes = dataService.data.attributes;
+        $scope.traits = dataService.data.traits;
     });
